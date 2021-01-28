e521def4436106a87025fc0367a790bea2f91871
Gang Lung Ken POON <poonyunkin04@gmail.com>
2021-01-28 16:16:54 +0800

    return JWT token in session call next with Error object if code 401 is occurred

--- server/routes/login.rest.js
+++ server/routes/login.rest.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const createError = require("http-errors");
-const body = require('express-validator');
-const userService = require("../service/userService");
-const jwtService = require("../service/jwtService");
+const express = require('express');
+const createError = require('http-errors');
+const {body} = require('express-validator');
+const userService = require('../service/userService');
+const jwtService = require('../service/jwtService');
 const router = express.Router();
 const url = '/login';
 
@@ -12,17 +12,17 @@ const url = '/login';
  * @returns JWT token
  */
 router.post(url, 
-    body('userName').not().isEmpty().withMessage('user name can\'t be empty '),
-    async(req, res, next) => {
-    const {userName, password} = req.body;
-    if (await userService.validateUser(userName, password)) {
-        try {
-            const jwtToken = jwtService.sign(userName)
-            return res.status(200).json({token: jwtToken});
-        } catch (err) {
-            console.log('jwt signing error: ' + err)
-            next(createError(401, "unexpected error occur"));
-        }
-    } else next(createError(401, "login failed, please try again"));
-});
+	body('userName').not().isEmpty().withMessage('user name can\'t be empty '),
+	async(req, res, next) => {
+		const {userName, password} = req.body;
+		if (await userService.validateUser(userName, password)) {
+			try {
+				const jwtToken = jwtService.sign(userName);
+				return res.status(200).cookie('Authorization', jwtToken, {httpOnly: true }).send();
+			} catch (err) {
+				console.log('jwt signing error: ' + err);
+				next(createError(400, 'unexpected error occur'));
+			}
+		} else next(new Error('login failed, please try again'));
+	});
 module.exports = router;
--- server/test/login.rest.test.js
+++ server/test/login.rest.test.js
@@ -1,65 +1,65 @@
-const { assert } = require("chai");
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../app.js");
-const { User } = require("../model/user");
-const jwt = require("jsonwebtoken");
+const { assert } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app.js');
+const { User } = require('../model/user');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const saltRound = Number(process.env.SALT_ROUND);
 const jwtSecret = process.env.JWT_SECRET;
-const prefix = "login.test_"
+const prefix = 'login.test_';
 
 chai.should();
 chai.use(chaiHttp);
 
-describe("/Login User", () => {
-    before(async () => {
-        const hashedPw = await bcrypt.hash("password", saltRound);
-        const user = new User({
-            userName: prefix + 'userName',
-            email: prefix + "email",
-            password: hashedPw
-        });
-        await user.save();
-    })
+describe('/Login User', () => {
+	before(async () => {
+		const hashedPw = await bcrypt.hash('password', saltRound);
+		const user = new User({
+			userName: prefix + 'userName',
+			email: prefix + 'email',
+			password: hashedPw
+		});
+		await user.save();
+	});
 
-    after(async () => {
-        await User.deleteMany({userName: prefix + 'userName'}).exec();        
-    })
+	after(async () => {
+		await User.deleteMany({userName: prefix + 'userName'}).exec();        
+	});
 
-    it("it should return a jwt token with code 200", done => {
-        chai
-          .request(app)
-          .post(`/rest/login`)
-          .send(
-              {
-                  "userName": prefix + "userName",
-                  "password": "password"
-                }
-            )
-          .end((err, res) => {
-            res.should.have.status(200);
-            const token = res.body.token;
-            const decoded = jwt.verify(token, jwtSecret);
-            assert.equal(decoded.userName, prefix + "userName")
-            done();
-          });
-      });
+	it('it should return a jwt token with code 200', done => {
+		chai
+			.request(app)
+			.post('/rest/login')
+			.send(
+				{
+					'userName': prefix + 'userName',
+					'password': 'password'
+				}
+			)
+			.end((err, res) => {
+				res.should.have.status(200);
+				const token = res.headers['set-cookie'][0].split(';')[0].split('=')[1];
+				const decoded = jwt.verify(token, jwtSecret);
+				assert.equal(decoded.userName, prefix + 'userName');
+				done();
+			});
+	});
 
-      it("it should return error 401, authentication failed", done => {
-        chai
-            .request(app)
-            .post(`/rest/login`)
-            .send(
-                {
-                    "userName": prefix + "userName-not-exit",
-                    "password": "password"
-                }
-            )
-            .end((err, res) => {
-            res.should.have.status(401);
-            res.body.error.should.have.property("message").eql('login failed, please try again');
-            done();
-            });
-        });
+	it('it should return error 401, authentication failed', done => {
+		chai
+			.request(app)
+			.post('/rest/login')
+			.send(
+				{
+					'userName': prefix + 'userName-not-exit',
+					'password': 'password'
+				}
+			)
+			.end((err, res) => {
+				res.should.have.status(401);
+				res.body.error.should.have.property('message').eql('login failed, please try again');
+				done();
+			});
+	});
 });
